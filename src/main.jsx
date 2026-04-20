94ec14cf18969b404e8b4beddbbfb44a12b418a7
xenobyte012 <xenobyte012@gmail.com>
2026-04-20 14:29:45 +0000

    fix basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
